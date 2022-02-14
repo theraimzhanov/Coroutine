38b929dcf7515f4e2025a975fd8b82a73c838878
theraimzhanov <90192137+theraimzhanov@users.noreply.github.com>
2022-02-14 16:51:08 +0600

    Initial commit

---
+++ app/src/main/java/com/raimzhanov/coroutine/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.raimzhanov.coroutine
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.core.view.isVisible
+import com.raimzhanov.coroutine.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+binding.buttonLoad.setOnClickListener {
+    loadData()
+}
+    }
+
+    private fun loadData() {
+        binding.progress.isVisible = true
+        binding.buttonLoad.isEnabled = false
+       loadCity{ it ->
+           binding.tvLocation.text = it
+           loadTemp(it){
+                binding.tvTemperature.text = it.toString()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
+        }
+    }
+
+    private fun loadTemp(city:String,callback: (Int) -> Unit) {
+        thread {
+            runOnUiThread {
+                Toast.makeText(this, "Loading temperature for city:$city", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            Thread.sleep(3000)
+
+           runOnUiThread {
+                callback.invoke(25)
+            }
+        }
+    }
+
+    private fun loadCity(callback:(String)->Unit) {
+        thread{
+            Thread.sleep(3000)
+            runOnUiThread {
+                callback.invoke("Bishkek")
+            }
+        }
+    }
+}
