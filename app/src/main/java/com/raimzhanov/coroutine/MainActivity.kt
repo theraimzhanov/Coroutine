1e78591c408e3a1c0b49cd9686c1d94cd8465189
theraimzhanov <90192137+theraimzhanov@users.noreply.github.com>
2022-02-17 18:14:43 +0600

    description coroutine// suspend

--- app/src/main/java/com/raimzhanov/coroutine/MainActivity.kt
+++ app/src/main/java/com/raimzhanov/coroutine/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         binding.buttonLoad.setOnClickListener {
             lifecycleScope.launch {
                 loadData()
+               // loadWithoutCoroutine()
             }
         }
     }
@@ -43,6 +44,32 @@ class MainActivity : AppCompatActivity() {
                 Log.d("handler", "loadData: finished : $this ")
 
     }
+    private fun loadWithoutCoroutine(step:Int = 0,obj:Any? = null){
+        when(step){
+            0->{
+                Log.d("handler", "loadData: started : $this ")
+                binding.progress.isVisible = true
+                binding.buttonLoad.isEnabled = false
+                 loadCityWithoutCoroutine {
+                     loadWithoutCoroutine(1,it)
+                 }
+            }
+            1->{
+                Log.d("handler", "loadData: middle 1 : $this ")
+                binding.tvLocation.text = obj.toString()
+                loadTempWithoutCoroutine(obj.toString()){
+                    loadWithoutCoroutine(2,it)
+                }
+            }
+            2->{
+                Log.d("handler", "loadData: middle 2 : $this ")
+                binding.tvTemperature.text = obj.toString()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+                Log.d("handler", "loadData: finished : $this ")
+            }
+        }
+    }
 
     private suspend fun loadTemp(city: String): Int {
         Toast.makeText(this, "Loading temperature for city:$city", Toast.LENGTH_SHORT)
@@ -55,4 +82,16 @@ class MainActivity : AppCompatActivity() {
         delay(3000)
         return "Bishkek"
     }
-}
+    private fun loadCityWithoutCoroutine(callback: (String)->Unit){
+        Handler(Looper.getMainLooper()).postDelayed({
+            callback.invoke("Bishkek")
+        },3000)
+    }
+    private fun loadTempWithoutCoroutine(city:String,callback: (Int)->Unit){
+                Toast.makeText(this, "Loading temperature for city:$city", Toast.LENGTH_SHORT)
+                    .show()
+            Handler(Looper.getMainLooper()).postDelayed({
+                callback.invoke(12)
+            },3000)
+        }
+        }
